23a171bf63fa299234043bc403753f079b1b9806
choyounnng <choyounnng@CHOXAEONIAN>
2025-01-16 23:53:46 +0900

    초기 커밋.

---
+++ Rental status/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rental_status
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Parent = button1; // label의 부모를 button1으로 설정
+            label2.BackColor = Color.Transparent; // 배경 투명 처리
+            label2.Location = new Point(
+                label2.Left - button1.Left, // 원래 위치 보정
+                label2.Top - button1.Top
+            );
+            label3.Parent = button2; // label의 부모를 button1으로 설정
+            label3.BackColor = Color.Transparent; // 배경 투명 처리
+            label3.Location = new Point(
+                label3.Left - button2.Left, // 원래 위치 보정
+                label3.Top - button2.Top
+            );
+            label4.Parent = button3; // label의 부모를 button1으로 설정
+            label4.BackColor = Color.Transparent; // 배경 투명 처리
+            label4.Location = new Point(
+                label4.Left - button3.Left, // 원래 위치 보정
+                label4.Top - button3.Top
+            );
+            label5.Parent = button4; // label의 부모를 button1으로 설정
+            label5.BackColor = Color.Transparent; // 배경 투명 처리
+            label5.Location = new Point(
+                label5.Left - button4.Left, // 원래 위치 보정
+                label5.Top - button4.Top
+            );
+            label6.Parent = button5; // label의 부모를 button1으로 설정
+            label6.BackColor = Color.Transparent; // 배경 투명 처리
+            label6.Location = new Point(
+                label6.Left - button5.Left, // 원래 위치 보정
+                label6.Top - button5.Top
+            );
+            label7.Parent = button6; // label의 부모를 button1으로 설정
+            label7.BackColor = Color.Transparent; // 배경 투명 처리
+            label7.Location = new Point(
+                label7.Left - button6.Left, // 원래 위치 보정
+                label7.Top - button6.Top
+            );
+            label8.Parent = button1; // label의 부모를 button1으로 설정
+            label8.BackColor = Color.Transparent; // 배경 투명 처리
+            label8.Location = new Point(
+                label8.Left - button1.Left, // 원래 위치 보정
+                label8.Top - button1.Top
+            );
+            label9.Parent = button2; // label의 부모를 button1으로 설정
+            label9.BackColor = Color.Transparent; // 배경 투명 처리
+            label9.Location = new Point(
+                label9.Left - button2.Left, // 원래 위치 보정
+                label9.Top - button2.Top
+            );
+            label10.Parent = button3; // label의 부모를 button1으로 설정
+            label10.BackColor = Color.Transparent; // 배경 투명 처리
+            label10.Location = new Point(
+                label10.Left - button3.Left, // 원래 위치 보정
+                label10.Top - button3.Top
+            );
+            label11.Parent = button4; // label의 부모를 button1으로 설정
+            label11.BackColor = Color.Transparent; // 배경 투명 처리
+            label11.Location = new Point(
+                label11.Left - button4.Left, // 원래 위치 보정
+                label11.Top - button4.Top
+            );
+            label12.Parent = button5; // label의 부모를 button1으로 설정
+            label12.BackColor = Color.Transparent; // 배경 투명 처리
+            label12.Location = new Point(
+                label12.Left - button5.Left, // 원래 위치 보정
+                label12.Top - button5.Top
+            );
+            label13.Parent = button6; // label의 부모를 button1으로 설정
+            label13.BackColor = Color.Transparent; // 배경 투명 처리
+            label13.Location = new Point(
+                label13.Left - button6.Left, // 원래 위치 보정
+                label13.Top - button6.Top
+            );
+            label14.Parent = button1; // label의 부모를 button1으로 설정
+            label14.BackColor = Color.Transparent; // 배경 투명 처리
+            label14.Location = new Point(
+                label14.Left - button1.Left, // 원래 위치 보정
+                label14.Top - button1.Top
+            );
+            label15.Parent = button2; // label의 부모를 button1으로 설정
+            label15.BackColor = Color.Transparent; // 배경 투명 처리
+            label15.Location = new Point(
+                label15.Left - button2.Left, // 원래 위치 보정
+                label15.Top - button2.Top
+            );
+            label16.Parent = button3; // label의 부모를 button1으로 설정
+            label16.BackColor = Color.Transparent; // 배경 투명 처리
+            label16.Location = new Point(
+                label16.Left - button3.Left, // 원래 위치 보정
+                label16.Top - button3.Top
+            );
+            label17.Parent = button4; // label의 부모를 button1으로 설정
+            label17.BackColor = Color.Transparent; // 배경 투명 처리
+            label17.Location = new Point(
+                label17.Left - button4.Left, // 원래 위치 보정
+                label17.Top - button4.Top
+            );
+            label18.Parent = button5; // label의 부모를 button1으로 설정
+            label18.BackColor = Color.Transparent; // 배경 투명 처리
+            label18.Location = new Point(
+                label18.Left - button5.Left, // 원래 위치 보정
+                label18.Top - button5.Top
+            );
+            label19.Parent = button6; // label의 부모를 button1으로 설정
+            label19.BackColor = Color.Transparent; // 배경 투명 처리
+            label19.Location = new Point(
+                label19.Left - button6.Left, // 원래 위치 보정
+                label19.Top - button6.Top
+            );
+
+            pictureBox2.Parent = button1; // pictureBox2의 부모를 button1으로 설정
+            pictureBox2.BackColor = Color.Transparent; // 배경 투명 처리
+            pictureBox2.Location = new Point(
+                pictureBox2.Left - button1.Left, // 원래 위치 보정
+                pictureBox2.Top - button1.Top    // 원래 위치 보정
+            );
+            pictureBox3.Parent = button2; // pictureBox2의 부모를 button1으로 설정
+            pictureBox3.BackColor = Color.Transparent; // 배경 투명 처리
+            pictureBox3.Location = new Point(
+                pictureBox3.Left - button2.Left, // 원래 위치 보정
+                pictureBox3.Top - button2.Top    // 원래 위치 보정
+            );
+            pictureBox4.Parent = button3; // pictureBox2의 부모를 button1으로 설정
+            pictureBox4.BackColor = Color.Transparent; // 배경 투명 처리
+            pictureBox4.Location = new Point(
+                pictureBox4.Left - button3.Left, // 원래 위치 보정
+                pictureBox4.Top - button3.Top    // 원래 위치 보정
+            );
+            pictureBox5.Parent = button4; // pictureBox2의 부모를 button1으로 설정
+            pictureBox5.BackColor = Color.Transparent; // 배경 투명 처리
+            pictureBox5.Location = new Point(
+                pictureBox5.Left - button4.Left, // 원래 위치 보정
+                pictureBox5.Top - button4.Top    // 원래 위치 보정
+            );
+            pictureBox6.Parent = button5; // pictureBox2의 부모를 button1으로 설정
+            pictureBox6.BackColor = Color.Transparent; // 배경 투명 처리
+            pictureBox6.Location = new Point(
+                pictureBox6.Left - button5.Left, // 원래 위치 보정
+                pictureBox6.Top - button5.Top    // 원래 위치 보정
+            );
+            pictureBox7.Parent = button6; // pictureBox2의 부모를 button1으로 설정
+            pictureBox7.BackColor = Color.Transparent; // 배경 투명 처리
+            pictureBox7.Location = new Point(
+                pictureBox7.Left - button6.Left, // 원래 위치 보정
+                pictureBox7.Top - button6.Top    // 원래 위치 보정
+            );
+
+            DateTime today = DateTime.Now;
+
+            DateTime threeDaysLater = today.AddDays(3);
+
+            label14.Text = $"{today.ToShortDateString()} ~ {threeDaysLater.ToShortDateString()}";
+            label15.Text = $"{today.ToShortDateString()} ~ {threeDaysLater.ToShortDateString()}";
+            label16.Text = $"{today.ToShortDateString()} ~ {threeDaysLater.ToShortDateString()}";
+            label17.Text = $"{today.ToShortDateString()} ~ {threeDaysLater.ToShortDateString()}";
+            label18.Text = $"{today.ToShortDateString()} ~ {threeDaysLater.ToShortDateString()}";
+            label19.Text = $"{today.ToShortDateString()} ~ {threeDaysLater.ToShortDateString()}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
---
+++ Rental status/Form2.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Rental_status
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::Rental_status.Properties.Resources.logo;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("돋움", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(206, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "3일 후";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("돋움", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(312, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "에 반납해주세요";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
